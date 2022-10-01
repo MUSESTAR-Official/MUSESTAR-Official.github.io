f81ccee6a77e426bf2154daa2552868c25ccb5c3
MUSESTARV <2633074007@qq.com>
2022-10-01 16:18:59 +0800

    更新

--- comic/files/cz2.js
+++ comic/files/cz2.js
@@ -1,7 +1,8 @@
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/01\">第1集<\/a><\/li>");
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/02\">第2集<\/a><\/li>");
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/03\">第3集<\/a><\/li>");
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/04\">第4集<\/a><\/li>");
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/05\">第5集<\/a><\/li>");
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/06\">第6集<\/a><\/li>");
-document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/07\">第7集<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/00\">第0话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/01\">第1话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/02\">第2话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/03\">第3话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/04\">第4话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/05\">第5话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/06\">第6话<\/a><\/li>");
+document.write("<li><a href=\"https:\/\/www.musestar.top\/comic\/cz\/07\">第7话<\/a><\/li>");
