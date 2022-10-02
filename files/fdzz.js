a19479b968c9eb5486a4ce3b9baad474e18eb8ad
MUSESTARV <2633074007@qq.com>
2022-10-02 20:51:43 +0800

    更新

--- files/fdzz.js
+++ files/fdzz.js
@@ -1,7 +1,7 @@
 document.write("    <div class=\"side r\">");
 document.write("    <div class=\"firs l\">");
 document.write("    <div class=\"dtit\">");
-document.write("        <h2>共收到128元<u><a href=\"https:\/\/www.musestar.top\/pay\" >发电赞助<\/a><\/u><\/h2>");
+document.write("        <h2>共收到128元<u><a href=\"https:\/\/www.musestar.top\/musestar\" >发电赞助<\/a><\/u><\/h2>");
 document.write("    <\/div>");
 document.write("    <\/div>");
 document.write("	<div class=\"tlist\">");
