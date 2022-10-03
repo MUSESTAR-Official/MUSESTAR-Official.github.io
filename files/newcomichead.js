46a2db7c1f2c7e1a34d3453c66eb8fc257cce4ef
MUSESTARV <2633074007@qq.com>
2022-10-03 13:28:23 +0800

    更新

--- files/newcomichead.js
+++ files/newcomichead.js
@@ -1,5 +1,5 @@
 document.write("	        <ul class=\"top_nav clearfix\">");
-document.write("                				<li class=\"active\"><a href=\"https:\/\/www.musestar.top\/comic\" >缪斯星动画<\/a><\/li>");
+document.write("                				<li class=\"active\"><a href=\"https:\/\/www.musestar.top\/comic\" >缪斯星漫画<\/a><\/li>");
 document.write("			<\/ul>");
 document.write("		<\/div>");
 document.write("		<div class=\"head_user\">");
@@ -7,7 +7,7 @@ document.write("		    <ul>");
 document.write("	        	<li><a href=\"https:\/\/www.musestar.top\/main\"><font size=\"1\">首页<\/font><\/a><\/li>");
 document.write("                <li><a href=\"https:\/\/www.musestar.top\/animation\" ><font size=\"1\">缪斯星动漫<\/font><\/a><\/li>");
 document.write("                <li><a href=\"https:\/\/www.musestar.top\/movie\" ><font size=\"1\">缪斯星影视<\/font><\/a><\/li>");
-document.write("                <li><a href=\"https:\/\/www.musestar.top\/comic\" ><font size=\"1\">缪斯星动画<\/font><\/a><\/li>");
+document.write("                <li><a href=\"https:\/\/www.musestar.top\/comic\" ><font size=\"1\">缪斯星漫画<\/font><\/a><\/li>");
 document.write("                <li><a href=\"https:\/\/www.musestar.top\/ecy\" ><font size=\"1\">二次元图库<\/font><\/a><\/li>");
 document.write("				<li><a href=\"https:\/\/www.musestar.top\/bbs\" ><font size=\"1\">缪斯星论坛<\/font><\/a><\/li>");
 document.write("				<li><a href=\"https:\/\/www.musestar.top\/musestar\" ><font size=\"1\">缪斯星官方<\/font><\/a><\/li>");
--- files/newmoviehead.js
+++ files/newmoviehead.js
@@ -7,7 +7,7 @@ document.write("		    <ul>");
 document.write("	        	<li><a href=\"https:\/\/www.musestar.top\/main\"><font size=\"1\">首页<\/font><\/a><\/li>");
 document.write("                <li><a href=\"https:\/\/www.musestar.top\/animation\" ><font size=\"1\">缪斯星动漫<\/font><\/a><\/li>");
 document.write("                <li><a href=\"https:\/\/www.musestar.top\/movie\" ><font size=\"1\">缪斯星影视<\/font><\/a><\/li>");
-document.write("                <li><a href=\"https:\/\/www.musestar.top\/comic\" ><font size=\"1\">缪斯星动画<\/font><\/a><\/li>");
+document.write("                <li><a href=\"https:\/\/www.musestar.top\/comic\" ><font size=\"1\">缪斯星漫画<\/font><\/a><\/li>");
 document.write("                <li><a href=\"https:\/\/www.musestar.top\/ecy\" ><font size=\"1\">二次元图库<\/font><\/a><\/li>");
 document.write("				<li><a href=\"https:\/\/www.musestar.top\/bbs\" ><font size=\"1\">缪斯星论坛<\/font><\/a><\/li>");
 document.write("				<li><a href=\"https:\/\/www.musestar.top\/musestar\" ><font size=\"1\">缪斯星官方<\/font><\/a><\/li>");
