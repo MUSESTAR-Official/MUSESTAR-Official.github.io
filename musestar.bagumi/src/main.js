9a07e196c7abd08412e4353e3c74f7a870797fd3
MUSESTARV <yxianxt@163.com>
2023-01-06 08:07:05 +0800

    更新

--- musestar.bagumi/src/main.js
+++
@@ -1,8 +0,0 @@
-import App from './App.svelte'
-import miniReset from './minireset.min.css'
-import iconfount from './assets/fontIcon/iconfont.css'
-const app = new App({
-  target: document.getElementById('app'),
-})
-
-export default app
