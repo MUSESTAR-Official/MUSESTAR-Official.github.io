09553ba1fccc6a351f693b5543cbe0eeba31988e
MUSESTARV <yxianxt@163.com>
2023-01-07 21:42:08 +0800

    更新

--- files/musehead.js
+++ files/musehead.js
@@ -8,20 +8,18 @@ document.write("			<li>");
 document.write("				<a href=\"https:\/\/www.musestar.top\/animation\">缪斯星动漫<\/a><\/li>");
 document.write("			<li>");
 document.write("				<a href=\"https:\/\/www.musestar.top\/movie\">缪斯星影视<\/a><\/li>");
-
 document.write("			<li>");
-document.write("				<a href=\" \">缪斯星暂无<\/a><\/li>");
+document.write("				<a href=\"http:\/\/www.musestars.top\">缪斯星搜索<\/a><\/li>");
 document.write("    <\/ul>");
 document.write("<ul class=\"dme r\">");
-
-document.write("			<li>");
-document.write("<a href=\"http:\/\/www.musestars.top\" >缪斯星搜索<\/a>");
-document.write("			<\/li>");
 document.write("			<li>");
 document.write("<a href=\"https:\/\/www.musestar.top\/musestar\" >缪斯星官方<\/a>");
 document.write("			<\/li>");
 document.write("			<li class=\"nav\">");
 document.write("<a href=\"https:\/\/www.musestar.top\/muse\" >缪斯星专区<\/a>");
+document.write("			<\/li>");
+document.write("			<li>");
+document.write("<a href=\" \" >缪斯星暂无<\/a>");
 document.write("			<\/li>");
 document.write("    <\/div>");
 document.write("	<br>");
