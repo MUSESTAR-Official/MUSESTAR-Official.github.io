a1ed0869446c724a2db42aee476dfdf33968712e
MUSESTARV <yxianxt@163.com>
2023-01-30 09:57:01 +0800

    更新

--- animation/files/history.js
+++ animation/files/history.js
@@ -1,5 +1,5 @@
 function isMobile(){
-	var sUserAgent= navigator.userAgent.toLowerCase(),  bIsIpad= sUserAgent.match(/ipad/i) == "ipad",  bIsIphoneOs= sUserAgent.match(/iphone os/i) == "iphone os",  bIsMidp= sUserAgent.match(/midp/i) == "midp",  bIsUc7= sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4",  bIsUc= sUserAgent.match(/ucweb/i) == "ucweb",  bIsAndroid= sUserAgent.match(/android/i) == "android",  bIsCE= sUserAgent.match(/windows ce/i) == "windows ce",  bIsWM= sUserAgent.match(/windows mobile/i) == "windows mobile",  bIsWebview = sUserAgent.match(/webview/i) == "webview";  return (bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM); 
+	var sUserAgent= navigator.userAgent.toLowerCase(),  bIsIpad= sUserAgent.match(/ipad/i) == "ipad",  bIsIphoneOs= sUserAgent.match(/iphone os/i) == "iphone os",  bIsMidp= sUserAgent.match(/midp/i) == "midp",  bIs夸克7= sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4",  bIs夸克= sUserAgent.match(/夸克web/i) == "夸克web",  bIsAndroid= sUserAgent.match(/android/i) == "android",  bIsCE= sUserAgent.match(/windows ce/i) == "windows ce",  bIsWM= sUserAgent.match(/windows mobile/i) == "windows mobile",  bIsWebview = sUserAgent.match(/webview/i) == "webview";  return (bIsIphoneOs || bIsMidp || bIs夸克7 || bIs夸克 || bIsAndroid || bIsCE || bIsWM); 
 }
 if (isMobile()) {
 	var curl=document.URL;
