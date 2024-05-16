b7f72fda8422ce1d6bb5dfc717596b0a731daf31
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2024-05-16 20:06:52 +0800

    更新

--- game/cdcxk/rank.php
+++ game/cdcxk/rank.php
@@ -113,6 +113,6 @@
       </div>
     </div>
   </footer>
-<script src="https://www.musestar.cc/files/dl.js"></script><a href="http://wfhsxx.musestar.cc/sy"> </a></body>
+<script src="https://www.musestar.cc/files/dl.js"></script></body>
 
 </html>
