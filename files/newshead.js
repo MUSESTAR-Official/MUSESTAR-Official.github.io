36ccf815d8978a98975444d51e8d96bc3b7e5495
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2024-08-22 02:24:06 +0800

    更新

--- files/newshead.js
+++ files/newshead.js
@@ -4,7 +4,7 @@ document.write("			<\/ul>");
 document.write("		<\/div>");
 document.write("		<div class=\"head_user\">");
 document.write("		    <ul>");
-document.write("	        	<li><a href=\"https:\/\/musestar.cc\"><font size=\"1\">首页<\/font><\/a><\/li>");
+document.write("	        	<li><a href=\"\/\"><font size=\"1\">首页<\/font><\/a><\/li>");
 document.write("                <li><a href=\"\/animation\" ><font size=\"1\">缪斯星动漫<\/font><\/a><\/li>");
 document.write("                <li><a href=\"\/movie\" ><font size=\"1\">缪斯星影视<\/font><\/a><\/li>");
 document.write("                <li><a href=\"http:\/\/search.musestar.cc\" ><font size=\"1\">缪斯星搜索<\/font><\/a><\/li>");
