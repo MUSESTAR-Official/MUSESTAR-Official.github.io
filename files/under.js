b97ed46e1d6659c64d38123ca83a66f43d728913
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2024-11-25 09:49:10 +0800

    Update under.js

--- files/under.js
+++ files/under.js
@@ -3,6 +3,7 @@ document.write("        <div class=\"footbg\">");
 document.write("           <a href=\" https:\/\/musestar.cc\/\">© 2024 MUSESTAR<\/a><br>");
 document.write("<a href=\"https:\/\/icp.gov.moe\/?keyword=20226956\" target=\"_blank\">萌ICP备20226956号<\/a><br>");
 document.write("<a href=\"https:\/\/stats.uptimerobot.com\/NJ9gBiMZ7D\" target=\"_blank\">缪斯星服务状态<\/a><br>");
+document.write("<a href=\"admin@musestar.cc\" target=\"_blank\">联系邮箱admin@musestar.cc<\/a><br>");
 document.write("<a>本站已运行 <span class=\"brand\" id=\"runtime\"><\/span><\/a><br>");
 document.write("<script type=\"text\/javascript\">");
 document.write("	function show_runtime(){");
