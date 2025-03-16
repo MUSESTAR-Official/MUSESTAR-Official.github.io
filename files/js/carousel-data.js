3ca25687439e8d2aaeca33de6313c7a784964907
MUSESTAR-Official <66924868+MUSESTAR-Official@users.noreply.github.com>
2025-03-16 22:49:12 +0800

    更新

--- files/js/carousel-data.js
+++ files/js/carousel-data.js
@@ -7,12 +7,17 @@ const carouselData = [
     },
     {
         image: "https://s1.imagehub.cc/images/2025/03/14/d1f465e94153189ea3a68f5c8e906947.png",
-        title: "我独自升级",
+        title: "我独自升级 第二季",
         url: "/animation/SoloLeveling2/01"
     },
     {
+        image: "https://s1.imagehub.cc/images/2025/03/16/c14b74c6e39aa9d5b39243864e6dc1a3.png",
+        title: "金牌得主",
+        url: "#"
+    },
+    {
         image: "https://s1.imagehub.cc/images/2025/03/14/a5e261e58c615b2b7b257e6241e805ea.png",
-        title: "Re:从零开始的异世界生活 第三季",
+        title: "关于我转生变成史莱姆这档事 第三季",
         url: "#"
     }
 ]; 
--- files/js/horizontal-anime-data.js
+++ files/js/horizontal-anime-data.js
@@ -2,32 +2,32 @@
 const horizontalAnimeData = [
     {
         image: "https://s1.imagehub.cc/images/2024/12/08/4448e7557b33850ac40959b8ac720130.jpg",
-        video: " ",
+        video: "https://my.microsoftpersonalcontent.com/personal/34ad44ca3c61e505/_layouts/15/download.aspx?share=EddkJSDIBCFAj6TXvZ9l8zwB0DUAxlfPD-6_GiO8DX6DKg",
         title: "BanG Dream! Ave Mujica",
         url: "/animation/AveMujica/01"
     },
     {
-        image: " ",
+        image: "https://s1.imagehub.cc/images/2024/12/16/d6fe24d2d22818bff757b88a6926353c.jpg",
         video: " ",
-        title: "测试",
-        url: "#"
+        title: "我独自升级 第二季",
+        url: "/animation/SoloLeveling2/01"
     },
     {
-        image: " ",
-        video: " ",
-        title: "测试",
-        url: "#"
+        image: "https://s1.imagehub.cc/images/2024/12/08/4448e7557b33850ac40959b8ac720130.jpg",
+        video: "https://my.microsoftpersonalcontent.com/personal/34ad44ca3c61e505/_layouts/15/download.aspx?share=EddkJSDIBCFAj6TXvZ9l8zwB0DUAxlfPD-6_GiO8DX6DKg",
+        title: "BanG Dream! Ave Mujica",
+        url: "/animation/AveMujica/01"
     },
     {
-        image: " ",
-        video: " ",
-        title: "测试",
-        url: "#"
+        image: "https://s1.imagehub.cc/images/2024/12/08/4448e7557b33850ac40959b8ac720130.jpg",
+        video: "https://my.microsoftpersonalcontent.com/personal/34ad44ca3c61e505/_layouts/15/download.aspx?share=EddkJSDIBCFAj6TXvZ9l8zwB0DUAxlfPD-6_GiO8DX6DKg",
+        title: "BanG Dream! Ave Mujica",
+        url: "/animation/AveMujica/01"
     },
     {
-        image: " ",
-        video: " ",
-        title: "测试",
-        url: "#"
-    }
+        image: "https://s1.imagehub.cc/images/2024/12/08/4448e7557b33850ac40959b8ac720130.jpg",
+        video: "https://my.microsoftpersonalcontent.com/personal/34ad44ca3c61e505/_layouts/15/download.aspx?share=EddkJSDIBCFAj6TXvZ9l8zwB0DUAxlfPD-6_GiO8DX6DKg",
+        title: "BanG Dream! Ave Mujica",
+        url: "/animation/AveMujica/01"
+    },
 ]; 
